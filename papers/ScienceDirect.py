c617e048db25639793e53daadc7d6b3f48be53d6
Dave Clements <clements@Clements-Galaxy.local>
2014-12-28 16:43:30 -0800

    Added ScienceDirect notfications.  Now supporting Web of Science and ScienceDirect.

---
+++ papers/ScienceDirect.py
@@ -0,0 +1,170 @@
+
+#!/usr/bin/python
+#
+# Information about a ScienceDirect reference / Citation
+
+
+import re
+import alert
+import base64
+import HTMLParser
+
+SD_SENDER = "salert@prod.sciencedirect.com"
+
+SD_JHU_PII_URL = "http://www.sciencedirect.com.proxy1.library.jhu.edu/science/article/pii/"
+SD_PII_URL = "http://www.sciencedirect.com/science/article/pii/"
+
+class SDPaper(alert.PaperAlert, HTMLParser.HTMLParser):
+    """
+    Describe a particular paper being reported by ScienceDirect
+    """
+
+    def __init__(self):
+        """
+
+        """
+        super(alert.PaperAlert,self).__init__()
+        HTMLParser.HTMLParser.__init__(self)
+        
+        self.title = ""
+        self.authors = ""
+        self.source = ""
+        self.doiUrl = ""
+        self.doi = ""
+        self.url = ""
+        self.search = "ScienceDirect: "
+        return None
+        
+    def getFirstAuthorLastName(self):
+        """
+        Dita Musalkova, Jakub Minks, Gabriela Storkanova, Lenka Dvorakova
+        This will mess up on van Drysdale etc.
+        """
+        if self.authors:
+            return(self.authors.split(",")[0].split(" ")[-1])
+        else:
+            return None
+
+    def getFirstAuthorLastNameLower(self):
+        firstAuthor = self.getFirstAuthorLastName()
+        if firstAuthor:
+            firstAuthor = firstAuthor.lower()
+        return firstAuthor
+
+
+        
+class SDEmail(alert.Alert, HTMLParser.HTMLParser):
+    """
+    All the information in a Science Direct Email alert.
+
+    Parse HTML email body from ScienceDirect.  The body maybe reporting more
+    than one paper.
+    """
+    searchStartRe = re.compile(r'Access (the|all \d+) new result[s]*')
+
+    def __init__(self, email):
+
+        HTMLParser.HTMLParser.__init__(self)
+
+        self.papers = []
+        self.search = "ScienceDirect: "
+        self.currentPaper = None
+        
+        self.inSearch = False
+        self.inTitleLink = False
+        self.inTitleText = False
+        self.afterTitleBeforeSource = False
+        self.inSource = False
+        self.afterSourceBeforeAuthors = False
+        self.inAuthors = False
+
+        # SD email body content is base64 encoded.  Decode it.
+        emailBodyText = base64.standard_b64decode(email.getBodyText())
+        # print(emailBodyText)
+        self.feed(emailBodyText) # process the HTML body text.
+        
+        return None
+        
+    def handle_data(self, data):
+
+        data = data.strip()
+
+        startingSearch = SDEmail.searchStartRe.match(data)
+        if startingSearch:
+            self.inSearch = True
+        elif self.inSearch:
+            self.search += data
+            self.inSearch = False
+        elif self.inTitleText:
+            self.currentPaper.title = data
+            self.inTitleText = False
+            self.afterTitleBeforeSource = True
+        elif self.inSource:
+            self.currentPaper.source = data
+            self.inSource = False
+            self.afterSourceBeforeAuthors = True
+        elif self.inAuthors:
+            self.currentPaper.authors = data
+            self.inAuthors = False
+        return(None)
+            
+    def handle_starttag(self, tag, attrs):
+
+        if tag == "td" and attrs[0][0] == "class" and attrs[0][1] == "txtcontent":
+            """
+            Paper has started; next tag is an anchor, and it has paper URL
+            We now have a long URL that points to a public HTML version of
+            the paper.  We don't have a doi. But we will have a title shortly.
+            Should we use the URL to get the DOI?  Or will sciencedirect
+            just always be title match?
+            ScienceDirect has an API we could use to extract the DOI, or we
+            could putll it from the HTML page.
+            For now, go with title only match
+            """
+            self.inTitleLink = True
+            self.currentPaper = SDPaper()
+            self.papers.append(self.currentPaper)
+
+        elif tag == "a" and self.inTitleLink:
+            fullUrl = attrs[0][1]
+            urlArgs = fullUrl.split("&")
+            for urlArg in urlArgs:
+                if urlArg[0:8] == "_piikey=":
+                    self.currentPaper.url = SD_PII_URL + urlArg[8:]
+                    break
+            inTitleLink = False
+        
+        elif tag == "span" and attrs[0][0] == "class" and attrs[0][1] == "artTitle":
+            self.inTitleText = True
+        elif tag == "i" and self.afterTitleBeforeSource:
+            self.inSource = True
+            self.afterTitleBeforeSource = False
+
+        elif tag == "span" and attrs[0][0] == "class" and attrs[0][1] == "authorTxt":
+            self.inAuthors = True
+            self.afterSourceBeforeAuthors = False
+
+            
+        return (None)
+
+    def handle_endtag(self, tag):
+
+        return (None)
+    
+    def handle_startendtag(self, tag, attrs):
+        """
+        Process tags like IMG and BR that don't have end tags.
+        """
+        return(None)
+        
+    def getPapers(self):
+        """
+        Return list of referencing papers in this alert.
+        """
+        return(self.papers)
+
+    def getSearch(self):
+        """
+        Returns text identifying what web os science search this alert is for.
+        """
+        return(self.search)
---
+++ papers/alert.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+#
+# Generic class to represent alerts.
+# Each alert describes a list of papers from a particular source, reported as a group.
+
+import re
+import HTMLParser
+
+
+class PaperAlert(object):
+    """
+    Abstract class for alerts from any source.
+    Class def serves as a template and hopes to intercept any method/attribute
+    references that aren't overridden by subclasses.
+    """
+
+    def __init__(self):
+        """
+        
+        """
+        self.title = None
+        self.authors = None
+        self.source = None
+        self.doiUrl = None
+        self.doi = None
+        self.url = None                   # a non-DOI URL
+        self.search = None
+        return None
+
+    def getTitleLower(self):
+        return(self.title.lower())
+        
+    def getFirstAuthorLastName(self):
+        """
+        Don't assume any consisten way to get this.  This had better be overriden.
+        """
+        return 1/0
+
+    def getFirstAuthorLastNameLower(self):
+        firstAuthor = self.getFirstAuthorLastName()
+        if firstAuthor:
+            firstAuthor = firstAuthor.lower()
+        return firstAuthor
+
+    def debugPrint(self):
+        print("Title: " + self.title)
+        print("Authors: " + self.authors)
+        print("Source: " + self.source)
+        print("DOI URL: " + self.doiUrl)
+        print("DOI: " + self.doi)
+        print("-----------------")
+        return(None)
+
+        
+class Alert(object):
+    """
+    All the information in an alert.
+    """
+
+    def __init__(self):
+
+        self.papers = []               # alerts must contain one or more papers
+        self.search = ""               # used to identify which alert papers are from
+        return None
+        
+
+    def getPapers(self):
+        """
+        Return list of referencing papers in this alert.
+        """
+        return(self.papers)
+
+    def getSearch(self):
+        """
+        Returns text identifying what web os science search this alert is for.
+        """
+        return(self.search)
