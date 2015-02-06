4a892ca5e05d13e6c53d1feb2909a1226b124102
Dave Clements <clements@Clements-Galaxy.local>
2015-02-06 02:15:04 -0800

    Added some support for reporting journals (not done yet). Completely rewrote and simplified library access code.  Now uses sets and intersections instead of compound indexes.  It's probably slower, but I can't tell, and the code is so much cleaner.

--- papers/generateCULReport.py
+++ papers/generateCULReport.py
@@ -10,50 +10,145 @@ import argparse
 
 import CiteULike                          # CiteULike Handling
 
+class FastCulLib(object):
+    """
+    Provides quick access to a citeulike library.
+    """
 
-def genMoinTagYearReport(byTagThenYear, byYear):
+    def __init__(self, culLib):
+        """
+        Given a CiteULike library, build quick index strucutures needed by
+        reports.
+        """
+        self.culLib = culLib
+
+        # These are replaced by frozensets at the end.
+        byYear = {}          # unordered array of papers from that year
+        byTag = {}           # value is unordered array of papers w/ tag
+        byJournal= {}        # unordered list of papers in each journal
+    
+        for paper in culLib.allPapers():
+
+            # Process Year
+            year = paper.getYear()
+            if year == "unknown":
+                print("Year UNKNOWN", paper.culJson) # Fix these when you find them.
+            if year not in byYear:
+                byYear[year] = []
+            byYear[year].append(paper)
+
+            # Process tags
+            tags = paper.getTags()                # every paper should have tags
+            for tag in tags:
+                if tag not in byTag:
+                    byTag[tag] = []
+                byTag[tag].append(paper)
+            if len(tags) == 0:
+                # should not happen, fix it when it happens.
+                print("Paper missing tags", paper.getTitle(), paper.getCulUrl())
+
+            # Process Journal
+            jrnl = paper.getJournalName()
+            if jrnl:
+                if jrnl not in byJournal:
+                    byJournal[jrnl] = []
+                byJournal[jrnl].append(paper) 
+
+        # create set versions
+        self.byYear = {}
+        for year in byYear:
+            self.byYear[year] = frozenset(byYear[year])
+        self.byTag = {}
+        for tag in byTag:
+            self.byTag[tag] = frozenset(byTag[tag])
+        self.byJournal = {}
+        for journal in byJournal:
+            self.byJournal[journal] = frozenset(byJournal[journal])
+        
+        return(None)
+
+    def getYears(self):
+        """
+        Return a list of years that papers were published in, in chronological
+        order.
+        """
+        return(sorted(self.byYear.keys()))
+
+    def getTags(self):
+        """
+        Return an unordered list of tags that exist in this lib.
+        """
+        return(self.byTag.keys())
+
+    def getJournals(self):
+        """
+        Return an unordered list of Journal names.
+        """
+        return(self.byJournal.keys())
+
+    def getPapers(self,
+                  tag = None,
+                  year = None,
+                  journal = None):
+        """
+        Given any combination of tag, year and.or journal, return the only the
+        set of papers that have the sepcified combination of values.
+        """
+        sets = []
+        if tag:
+            sets.append(self.byTag[tag])
+        if year:
+            sets.append(self.byYear[year])
+        if journal:
+            sets.append(self.byJournal[journal])
+
+        if len(sets) == 0:
+            return(culLib.allPapers())
+        elif len(sets) == 1:
+            return(sets[0])
+        else:
+            narrowed = sets[0]
+            for restriction in sets[1:]:
+                narrowed = narrowed.intersection(restriction)
+            return(narrowed)
+
+
+def genMoinTagYearReport(fastCulLib):
     """
     Generate a papers by tag and year report in MoinMoin markup.
     Report is returned as a multi-line string.
     """
     # Preprocess. Need to know order of tags and years.
-    nTags = len(byTagThenYear)
-    years = sorted(byYear.keys())  # years are listed chronologically
-
+    tags = fastCulLib.getTags()
     # Count number of papers with each tag
     nPapersWTag = {}
-    for tag in byTagThenYear.keys():
-        nPapersWTag[tag] = 0
-    for tag, tagYears in byTagThenYear.items():
-        for year, papers in tagYears.items():
-            nPapersWTag[tag] += len(papers)
+    for tag in tags:
+        nPapersWTag[tag] = len(fastCulLib.getPapers(tag=tag))
 
     # sort tags by paper count, max first
     tagsInCountOrder = [tag for tag in
                         sorted(nPapersWTag.keys(),
                                key=lambda keyValue: - nPapersWTag[keyValue])]
 
-    # no have everything we need; generate report        
-    report = []
-
+    report = []                # now have everything we need; generate report
+    
     # generate header
     report.append('||<|2 class="th"> Year ' +
-                  '||<-' + str(nTags) + ' class="th"> Tags ||' +
+                  '||<-' + str(len(tags)) + ' class="th"> Tags ||' +
                   '||<|2 class="th"> # ||\n')
 
     for tag in tagsInCountOrder:
         report.append('||<class="th"> ' + tag + ' ')
     report.append('||\n')
 
-    # generate yearly numbers
-    nPapers = 0
-    for year in years:
-        nPapersThisYear = len(byYear[year])
-        nPapers += nPapersThisYear
+    # generate numbers per year
+    for year in fastCulLib.getYears():  # years are listed chronologically
+        nPapersThisYear = len(fastCulLib.getPapers(year=year))
         report.append('||<class="th"> ' + year + ' ')
         for tag in tagsInCountOrder:
-            if year in byTagThenYear[tag]:
-                count = str(len(byTagThenYear[tag][year]))
+            papersForTagYear = fastCulLib.getPapers(tag=tag, year=year)
+            if papersForTagYear:
+                count = str(len(papersForTagYear))
             else:
                 count = ""
             report.append('||<)> ' + count + ' ')
@@ -61,102 +156,85 @@ def genMoinTagYearReport(byTagThenYear, byYear):
 
     # generate total line at bottom
     report.append('||<class="th"> Total ||<) class="th"> ' +
-                  str(nPapers) + ' ')
+                  str(len(fastCulLib.getPapers())) + ' ')
     for tag in tagsInCountOrder:
         report.append('||<) class="th"> ' + str(nPapersWTag[tag]) + ' ')
     report.append('||\n')
 
     return("".join(report))
-        
 
-class Argghhs(object):
+
+    
+def argghhs():
     """
     Process and provide access to command line arguments.
     """
 
-    def __init__(self):
-        argParser = argparse.ArgumentParser(
-            description="Generate reports for the CiteULike Galaxy library.")
-        argParser.add_argument(
-            "-c", "--cullib", required=True,
-            help="JSON formatted file containing CiteUlike Library; obtained by going to http://www.citeulike.org/json/group/16008")
-        argParser.add_argument(
-            "--tagyear", required=False, action="store_true",
-            help="Produce table showing number of papers with each tag, each year.")
-        argParser.add_argument(
-            "--moin", required=False, action="store_true",
-            help="Produce report(s) using MoinMoin markup")
-        argParser.add_argument(
-            "--csv", required=False,
-            help=("Produce report(s) in CSV format"))
-        self.args = argParser.parse_args()
+    argParser = argparse.ArgumentParser(
+        description="Generate reports for the CiteULike Galaxy library.")
+    argParser.add_argument(
+        "-c", "--cullib", required=True,
+        help="JSON formatted file containing CiteUlike Library; obtained by going to http://www.citeulike.org/json/group/16008")
+    argParser.add_argument(
+        "--tagyear", required=False, action="store_true",
+        help="Produce table showing number of papers with each tag, each year.")
+    argParser.add_argument(
+        "--journalyear", required=False, action="store_true",
+        help="Produce table showing number of papers in different journals, each year.")
+    argParser.add_argument(
+        "--moin", required=False, action="store_true",
+        help="Produce report(s) using MoinMoin markup")
+    argParser.add_argument(
+        "--csv", required=False,
+        help=("Produce report(s) in CSV format"))
 
-        return(None)
+    return(argParser.parse_args())
 
 
         
 # =============================================================
 # MAIN
 
-args = Argghhs()                          # process command line arguments
-# print str(args.args)
+args = argghhs()                          # process command line arguments
+# print str(args)
 
 # create database from CiteULike Library.
-culLib = CiteULike.CiteULikeLibrary(args.args.cullib)
+culLib = CiteULike.CiteULikeLibrary(args.cullib)
 
-# Basically want papers categorized by tag and year.  And we'll want a
-# way to get papers
-#  by year and then tag
-#  by tag and then year
-# Do we actually care about the papers?  Probably not, but just as easy to
-# keep the papers as to keep a count of them.
-# So keep papers in tag/year, just as a list.
-# then have year be a dict of tags that point the same list.
-# and vice versa
+fastCulLib = FastCulLib(culLib)
 
-byTagThenYear = {}
-byYearThenTag = {}
-byYear = {}
+if args.tagyear:
 
-for paper in culLib.allPapers():
-    year = paper.getYear()
+    # Generate them reports
 
-    if year == "unknown":
-        print("Year UNKNOWN", paper.culJson)
-    if year not in byYear:
-        byYear[year] = []
-        byYearThenTag[year] = {}
-    byYear[year].append(paper)
-        
-    tags = paper.getTags()                # every paper should have tags
-    for tag in tags:
-        if tag not in byTagThenYear:
-            byTagThenYear[tag] = {}         # will be indexed by year
-        if year not in byTagThenYear[tag]:
-            byTagThenYear[tag][year] = []   # unordered list of papers
-            # use the same list that byTagThenYear entry points at.
-            byYearThenTag[year][tag] = byTagThenYear[tag][year]
-        byTagThenYear[tag][year].append(paper)
-    if len(tags) == 0:
-        print("Paper missing tags", paper.getTitle(), paper.getCulUrl())
+    # options are to have one routine per report/format combo, or create a Moin
+    # report, or a csv report and then have their separate methods do the dirty
+    # work. Try that.
 
-
-# All papers indexed by [tag][year] and [year][tag]
-
-# Generate them reports
-
-# options are to have one routine per report/format combo, or create a Moin
-# report, or a csv report and then have their separate methods do the dirty
-# work. Try that.
-
-if args.args.tagyear:
     # report showing papers by tag by year requested.
-    if args.args.moin:
+    if args.moin:
         # generate a tag year report in MoinMoin format.
-        moinReport = genMoinTagYearReport(byTagThenYear, byYear)
+        moinReport = genMoinTagYearReport(fastCulLib)
         print(moinReport)
-    if args.args.csv:
+    if args.csv:
         # generate tag year data in a tab delimited file
-        csvReport = CsvTagYearReport(byTagThenYear)
+        csvReport = genCsvTagYearReport(fastCulLib)
         print(csvReport)
 
+if args.journalyear:
+    # Count how many papers appeared in each jounal in each year.
+    # Generate CSV here?  Need to sort it somehow.  X axis should be year.
+    # Y axis Journal.  Put the journal with the most all time pubs at the top
+    # And also include publisher as we care about BMC.
+
+    for year in fastCulLib.getYears():
+        for journal in fastCulLib.getJournals():
+            papers = fastCulLib.getPapers(journal=journal, year=year)
+            if papers:
+                print("year/Journal/nPapers ", year, journal, len(papers))
+
+    print("=====================")
+
+    for journal in fastCulLib.getJournals():
+        print("Journal total", journal, len(fastCulLib.getPapers(journal=journal)))
+                
